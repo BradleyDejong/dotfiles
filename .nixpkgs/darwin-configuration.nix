0c2bebf95d23e80c5c877c0f3b008cf64a21e309
Bradley DeJong <DeJong.Bradley@mayo.edu>
2020-06-06 20:53:03 -0500

    Add nix-darwin configuration

---
+++ .nixpkgs/darwin-configuration.nix
@@ -0,0 +1,70 @@
+{ config, pkgs, ... }:
+
+let
+  tmux = import ~/.nixpkgs/tmux (with pkgs;
+    { inherit
+        makeWrapper
+	symlinkJoin
+	writeText
+	;
+      tmux = pkgs.tmux;
+    });
+
+in
+
+{
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages =
+    [ pkgs.vim
+      pkgs.httpie
+      pkgs.neovim
+      pkgs.git
+      pkgs.zsh
+      pkgs.antibody
+      pkgs.oh-my-zsh
+      pkgs.nodejs-10_x
+      tmux
+      pkgs.tmuxinator
+      pkgs.wget
+      pkgs.fd
+      pkgs.fzf
+      pkgs.ripgrep
+      pkgs.tree
+      pkgs.pandoc
+      pkgs.pass
+      pkgs.rig
+      pkgs.lolcat
+      pkgs.toilet
+      pkgs.gnupg
+      pkgs.sl
+      pkgs.mono6
+      # pkgs.dotnet-sdk
+      pkgs.graphviz
+    ];
+
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+
+  # environment.allowUnsupportedSystem = true;
+
+  # Auto upgrade nix package and the daemon service.
+  # services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  # Create /etc/bashrc that loads the nix-darwin environment.
+  # programs.bash.enable = true;
+  programs.zsh.enable = true;
+  # programs.fish.enable = true;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+
+  # You should generally set this to the total number of logical cores in your system.
+  # $ sysctl -n hw.ncpu
+  nix.maxJobs = 1;
+  nix.buildCores = 1;
+}
